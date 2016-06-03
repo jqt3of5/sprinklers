a39a5db0c59663624f005d190fe636958ab57c0f
John Todd <jqtodd@xactware.com>
2016-06-03 16:20:09 -0600

    Added the web pages for the dashboard.

---
+++ web_pages/app.js
@@ -0,0 +1,68 @@
+window.onload = function()
+{
+    device_buttons = [document.getElementById("thermometer-button"),document.getElementById("garage-button"), document.getElementById("sprinkler-button")];
+    refreshDevices();
+    window.setInterval(refreshDevices, 1000);
+}
+
+var device_buttons = []; 
+var devices = [];
+
+function refreshDevices()
+{
+    //Hit the endpoint
+    //Update the global variable
+    //Update the UI
+    for(var i = 0; i < device_buttons.length; ++i)
+    {
+	device_buttons[i].disabled = true;
+    }
+}
+
+function refreshTempuratures()
+{
+    //It's possible this device has disconnected, kick back to devices screen
+
+    //Search through devices for temp device
+}
+
+function refreshGarage()
+{
+    //It's possible this device has disconnected, kick back to devices screen
+    
+    //Search through devices for garage device
+}
+
+function tempClick()
+{
+    var list = document.getElementById("device-list");
+    var temp = document.getElementById("thermometer");
+
+    list.style.display = "none";
+    temp.style.display = "block";
+
+    window.setInterval(refreshTempuratures, 2000);
+}
+
+function garageClick()
+{
+    var list = document.getElementById("device-list");
+    var garage = document.getElementById("garage");
+
+    list.style.display = "none";
+    garage.style.display = "block";
+
+    window.setInterval(refreshGarage, 500);
+}
+
+function toggleGarageClick()
+{
+}
+
+function toggleLightClick()
+{
+}
+
+function sprinklerClick()
+{
+}
