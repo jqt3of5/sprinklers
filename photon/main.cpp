8f84619a4a0a101075dd044bb5623969f06067be
unknown <jqt3of5@gmail.com>
2016-06-04 11:38:39 -0600

    Got the binary protocol down, moving to something more friendly.

--- photon/main.cpp
+++ photon/main.cpp
@@ -3,6 +3,7 @@
 #include <softap_http.h>
 
 const int EEPROM_HOST_ADDR = 0;
+const int EEPROM_SCHEDULE = 4;
 
 struct Page
 {
@@ -11,17 +12,27 @@ struct Page
   int (*handler)(char ** urls, char ** keys, char ** values, char*&response);
 };
 
+struct IpAddr
+{
+  byte ip[4];
+};
+
+struct Schedule
+{
+  char data[64];
+};
+
 int handle_config(char ** urls, char ** keys, char**values, char * &response)
 {
   for (int i = 0; keys[i] != nullptr; ++i)
   {
     if (!strcmp(keys[i], "ip"))
     {
-      char ip[4] = {0};
-      ip[0] = atoi(strtok(values[i], "."));
+      IpAddr ip;
+      ip.ip[0] = atoi(strtok(values[i], "."));
       for (int i = 1; i < 4; ++i)
       {
-        ip[i] = atoi(strtok(0, "."));
+        ip.ip[i] = atoi(strtok(0, "."));
       }
       EEPROM.put(EEPROM_HOST_ADDR, ip);
     }
@@ -104,36 +115,116 @@ void http_handler(const char* url, ResponseCallback* cb, void* cbArg, Reader*bod
 
 STARTUP(softap_set_application_page_handler(http_handler, nullptr));
 
+bool _connected = false;
 TCPClient client;
 void setup()
 {
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
   pinMode(D7, OUTPUT);
 
-  byte addr[4] = {0};
-  EEPROM.get(EEPROM_HOST_ADDR, addr);
-  if (addr[0] == 0xFF)
+  IpAddr ip;
+  EEPROM.get(EEPROM_HOST_ADDR, ip);
+  if (ip.ip[0] == 0xFF)
   {
     //If no address is set, start listening.
     WiFi.listen();
   }
-  else if (!client.connect(addr, 8081))
+  else if (!client.connect(ip.ip, 8081))
   {
     //If connecting failed, fall back to listening mode
     WiFi.listen();
   }
+  else
+  {
+    _connected = true;
+  }
+}
 
+char processData(char * data, int count)
+{
+    char result = 0;
+    for (int i = 0; i < count; ++ i)
+    {
+      char cmd = (data[i] >> 6) & 0x03;
+      //The command argument
+      char arg;
+
+      Schedule schedule;
+      switch (cmd)
+      {
+        case 0:
+        //write schedule
+        //00cccccc .......
+        //move to the start of the data;
+        arg = data[i] & 0x3F;
+        memcpy(&schedule, &data[i+1], arg);
+        EEPROM.put(EEPROM_SCHEDULE, schedule);
+        i += arg + 1;
+        break;
+
+        case 1:
+        result = 0;
+        //get pins state
+        //010ppp00
+        arg = (data[i] >> 2) & 0x0F;
+        for (int j = 0; j < 8; ++j)
+        {
+          result |= digitalRead(j) << j;
+        }
+        break;
+
+        case 2:
+        //set pin state
+        //100ppp0x
+        arg = (data[i] >> 2) & 0x0F;
+        digitalWrite(arg, data[i] & 0x03);
+        break;
+
+        case 3:
+        break;
+      }
+
+    }
+    return result;
 }
 
 void loop()
 {
-  //char addr[4] = {0};
-  //EEPROM.get(EEPROM_HOST_ADDR, addr);
   if (client.connected())
   {
     //client.printf("%d.%d.%d.%d", addr[0], addr[1],addr[2],addr[3]);
-    while (client.available())
+    int total = client.available();
+    if (total)
     {
-      client.read();
+      char * data = new char[total]();
+
+      for (int i = 0; client.available(); ++i)
+      {
+        data[i] = client.read();
+        ++i;
+      }
+      //Process Data
+      char result = processData(data, total);
+      client.write(result);
     }
   }
+  else if (_connected)
+  {
+    IpAddr ip;
+    EEPROM.get(EEPROM_HOST_ADDR, ip);
+    //Try once to reconnect
+    if (!client.connect(ip.ip, 8081))
+    {
+      _connected = false;
+      WiFi.listen();
+    }
+  }
+
+
 }
