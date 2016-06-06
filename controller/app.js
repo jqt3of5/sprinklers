1ef6d14a9f59f05240ab0c3b4df903f769ea4b01
jqt3of5 <jqt3of5@gmail.com>
2016-06-06 13:28:46 -0600

    Lots of important changes/addition. Multiple device support, refactor

--- controller/app.js
+++ controller/app.js
@@ -21,62 +21,188 @@ var uuid = require('node-uuid');
 
 app.use(express.static('public'));
 
-//Allocate a new pin
-/*app.put('/:host/gpio/:pin', function (req, res) {
-    var host = req.params.host;
-    var pin = parseInt(req.params.pin);
-    var cmd = [0x00 << 6 | pin << 1 | 0];
-
-    gpio.send(host, cmd, function(data, error){
-        res.end();
-    });
-});
-
-//unallocate a pin
-app.delete('/:host/gpio/:pin', function(req, res) {
-    res.end();
-});
-*/
 //Get the value of a pin. 
-app.get('/:deviceId/gpio/:pin', function(req, res) {
+app.get('/:deviceId/:pin', function(req, res) {
 	var deviceId = req.params.deviceId;
 	var pin = parseInt(req.params.pin);
 	var cmd = "R " + pin;
 	
-	var deviceIp = device_infos[deviceId].ip;
-	var deviceSocket = device_connections[deviceIp].socket;
-	
-	deviceSocket.on('data', function (data) {
-		//var state = JSON.parse(data);
-		res.end(data);
-	});
-	deviceSocket.write(cmd);
+	if (device_infos[deviceId] == undefined)
+	{
+		res.status(404).end();
+	}
+	else
+	{
+		var deviceSocket = device_infos[deviceId].socket;
+		var dataHandler = function (data) {
+			res.end(data);
+			deviceSocket.removeListener('data', dataHandler);
+		};
+		
+		deviceSocket.on('data', dataHandler);
+		deviceSocket.write(cmd);	
+	}
 });
 
-//Set the value of a pin to low
-app.post('/:deviceId/gpio/:pin/clr', function(req, res) {
+//Set the value of a pin
+app.post('/:deviceId/:pin', function(req, res) {
 	var deviceId = req.params.deviceId;
-    var pin = parseInt(req.params.pin);
-    var cmd = "W " + pin + " 0";
+    	var pin = parseInt(req.params.pin);
+    	var cmd = "W " + pin + " " + req.body;
+    	
+	if (device_infos[deviceId] == undefined)
+	{
+		res.status(404).end();	
+	}
+	else
+	{
+		var deviceSocket = device_infos[deviceId].socket;
 	
-	var deviceIp = device_infos[deviceId].ip;
-	var deviceSocket = device_connections[deviceIp].socket;
-	
-	deviceSocket.write(cmd);
-	res.end();
+		deviceSocket.write(cmd);
+		res.end();
+	}
 });
 
-//set the value of a pin to high
-app.post('/:deviceId/gpio/:pin/set', function(req, res) {
-    var deviceId = req.params.deviceId;
-    var pin = parseInt(req.params.pin);
-    var cmd = "W " + pin + " 1";
+app.post('/:deviceId/schedule', function(req, res){
+	var deviceId = req.params.deviceId;
+    	var pin = parseInt(req.params.pin);
+    	var cmd = "S" + req.body;
+    	
+    	if (device_infos[deviceId] == undefined)
+    	{
+    		res.status(404).end();
+    	}
+    	else if (device_infos[deviceId].type != "relay")
+    	{
+    		res.status(405).end();	
+    	}
+    	else 
+	{
+		var deviceSocket = device_infos[deviceId].socket;
 	
-	var deviceIp = device_infos[deviceId].ip;
-	var deviceSocket = device_connections[deviceIp].socket;
+		deviceSocket.write(cmd);
+		res.end();
+	}
+});
+
+app.get('/:deviceId/temp/:probe', function(req, res) {
+	var deviceId = req.params.deviceId;
+	var probe = parseInt(req.params.probe);
+	var cmd = "T " + probe;
 	
-	deviceSocket.write(cmd);
-	res.end();
+	if (device_infos[deviceId] == undefined)
+	{
+		res.status(404).end();
+	}
+	else if (device_infos[deviceId].type != "temp")
+	{
+		res.status(405).end();
+	} 
+	else 
+	{
+		var deviceSocket = device_infos[deviceId].socket;
+		var dataHandler = function (data) {
+			res.end(data);
+			deviceSocket.removeListener('data', dataHandler);
+		};
+		
+		deviceSocket.on('data', dataHandler);
+		deviceSocket.write(cmd);	
+	}
+});
+
+app.get('/:deviceId/garage/door', function(req, res) {
+	var deviceId = req.params.deviceId;
+	//door, state
+	var cmd = "D S";
+	
+	if (device_infos[deviceId] == undefined)
+	{
+		res.status(404).end();
+	}
+	else if (device_infos[deviceId].type != "garage")
+	{
+		res.status(405).end();
+	} 
+	else
+	{
+		var deviceSocket = device_infos[deviceId].socket;
+		var dataHandler = function (data) {
+			res.end(data);
+			deviceSocket.removeListener('data', dataHandler);
+		};
+		
+		deviceSocket.on('data', dataHandler);
+		deviceSocket.write(cmd);		
+	}
+});
+
+app.get('/:deviceId/garage/light', function(req, res) {
+	var deviceId = req.params.deviceId;
+	//Light, State
+	var cmd = "L S";
+	
+	if (device_infos[deviceId] == undefined)
+	{
+		res.status(404).end();
+	}
+	else if (device_infos[deviceId].type != "garage")
+	{
+		res.status(405).end();
+	} 
+	else
+	{
+		var deviceSocket = device_infos[deviceId].socket;
+		var dataHandler = function (data) {
+			res.end(data);
+			deviceSocket.removeListener('data', dataHandler);
+		};
+		
+		deviceSocket.on('data', dataHandler);
+		deviceSocket.write(cmd);		
+	}
+});
+
+app.post('/:deviceId/garage/light', function(req, res) {
+	var deviceId = req.params.deviceId;
+	//Light, Toggle
+	var cmd = "L T";
+	
+	if (device_infos[deviceId] == undefined)
+	{
+		res.status(404).end();
+	}
+	else if (device_infos[deviceId].type != "garage")
+	{
+		res.status(405).end();
+	} 
+	else
+	{
+		var deviceSocket = device_infos[deviceId].socket;
+		deviceSocket.write(cmd);
+		res.end();
+	}
+});
+
+app.post('/:deviceId/garage/door', function(req, res) {
+	var deviceId = req.params.deviceId;
+	//Door, Toggle
+	var cmd = "D T";
+	
+	if (device_infos[deviceId] == undefined)
+	{
+		res.status(404).end();
+	}
+	else if (device_infos[deviceId].type != "garage")
+	{
+		res.status(405).end();
+	} 
+	else
+	{
+		var deviceSocket = device_infos[deviceId].socket;
+		deviceSocket.write(cmd);
+		res.end();
+	}
 });
 
 app.get('/devices', function(req, res) {
@@ -87,30 +213,24 @@ var http_server = app.listen(8080, function(){
     console.log("Started listening");			
 });
 
-var device_connections = {};
 var device_infos = {};
-
 var socket_server = net.createServer(function (socket)
 {
-	socket.on('data', function (data)
+	function configDevice(data)
 	{
+		var device_info = JSON.parse(data);
 		//When the device first connects, it should send an object describing itself. name/type
-		if (device_connections[socket.remoteAddress] == undefined)
-		{
-			var device_info = JSON.parse(data);
-			device_connections[socket.remoteAddress] = {socket:socket, deviceId:device_info.deviceId};
-			device_infos[device_info.deviceId] = {ip:socket.remoteAddress, type:device_info.type};	
-		}
-	});
-	
-	socket.on('end', function ()
-	{
-		//I don't know if this will work, because remoteAddress is supposed to be undefined when the client disconnects
-		//var device = connected_devices[socket.remoteAddress];
-		//connected_devices[socket.remoteAddress] = undefined;
-		//device_infos[device.deviceId] = undefined;
+		socked.deviceId = device_info.deviceId;
+		device_infos[device_info.deviceId] = {socket:socket, type:device_info.type};	
 		
-	})
+		socket.removeListener('data', configDevice);
+	}
+	socket.on('data', configDevice);
+	
+	socket.on('close', function ()
+	{
+		device_infos[socket.deviceId] = undefined;
+	});
 });
 
 socket_server.listen(8081, function(){ console.log("Server Started")});
