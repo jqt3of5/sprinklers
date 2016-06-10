74aa37e3141b516ed34df58df00ff76c5be1c34c
jqt3of5 <jqt3of5@gmail.com>
2016-06-10 17:43:06 -0600

    Update app.js

--- controller/app.js
+++ controller/app.js
@@ -11,7 +11,6 @@
 ///=============================================================================================================
 
 var express = require('express');
-var app = express();
 var fs = require('fs');
 var net = require('net');
 var uuid = require('node-uuid');
@@ -22,33 +21,30 @@ DeviceTypeEnum ={
 	Relay : "relay"
 };
 
-var endpoints = [{url:"/:deviceId/schedule",     method:"GET",  allowedDevices:[DeviceTypeEnum.Relay],  createCommand:function(req) {return "S R"}},
-		 {url:"/:deviceId/schedule",     method:"POST", allowedDevices:[DeviceTypeEnum.Relay],  createCommand:function(req) {return "S W" + req.body}, },
-		 {url:"/:deviceId/garage/door",  method:"GET",  allowedDevices:[DeviceTypeEnum.Garage], createCommand:function(req) {return "D S"}, },
-		 {url:"/:deviceId/garage/door",  method:"POST", allowedDevices:[DeviceTypeEnum.Garage], createCommand:function(req) {return "D T"}, },
-		 {url:"/:deviceId/garage/light", method:"POST", allowedDevices:[DeviceTypeEnum.Garage], createCommand:function(req) {return "L T"}, },
-		 {url:"/:deviceId/garage/light", method:"GET", allowedDevices:[DeviceTypeEnum.Garage], createCommand:function(req) {return "L S"}, },
-		 {url:"/:deviceId/garage/config", method:"POST",  allowedDevices:[DeviceTypeEnum.Garage], createCommand:function(req) {return "L O "+req.body}, },
-		 {url:"/:deviceId/temp/:probe",  method:"GET",  allowedDevices:[DeviceTypeEnum.Temp],   createCommand:function(req) {return "T " + req.params.probe}, },
-		 {url:"/:deviceId/gpio/:pin",         method:"GET",  allowedDevices:[], createCommand:function(req) {return "R " + req.params.pin} },
-		 {url:"/:deviceId/gpio/:pin/:value",  method:"POST", allowedDevices:[], createCommand:function(req) {return "W " + req.params.pin + " " + req.params.value} },
+var endpoints = [{url:"/:deviceId/relay/schedule",     method:"GET",  allowedDevices:[DeviceTypeEnum.Relay],  createCommand:function(params, body) {return "S R"}},
+		 {url:"/:deviceId/relay/schedule",     method:"POST", allowedDevices:[DeviceTypeEnum.Relay],  createCommand:function(params, body) {return "S W" + body}, },
+		 {url:"/:deviceId/garage/door",  method:"GET",  allowedDevices:[DeviceTypeEnum.Garage], createCommand:function(params, body) {return "D S"}, },
+		 {url:"/:deviceId/garage/door",  method:"POST", allowedDevices:[DeviceTypeEnum.Garage], createCommand:function(params, body) {return "D T"}, },
+		 {url:"/:deviceId/garage/light", method:"POST", allowedDevices:[DeviceTypeEnum.Garage], createCommand:function(params, body) {return "L T"}, },
+		 {url:"/:deviceId/garage/light", method:"GET", allowedDevices:[DeviceTypeEnum.Garage], createCommand:function(params, body) {return "L S"}, },
+		 {url:"/:deviceId/garage/config", method:"POST",  allowedDevices:[DeviceTypeEnum.Garage], createCommand:function(params, body) {return "L O "+body}, },
+		 {url:"/:deviceId/temp/:probe",  method:"GET",  allowedDevices:[DeviceTypeEnum.Temp],   createCommand:function(params, body) {return "T " + params.probe}, },
+		 {url:"/:deviceId/gpio/:pin",         method:"GET",  allowedDevices:[], createCommand:function(params, body) {return "R " + params.pin} },
+		 {url:"/:deviceId/gpio/:pin/:value",  method:"POST", allowedDevices:[], createCommand:function(params, body) {return "W " + params.pin + " " + params.value} },
 ];
 
-app.use(express.static('public'));
-
-app.get('/devices', function(req, res) {
-	res.end(JSON.stringify(device_infos));
-});
-
+var app = express();
+//Immediately Invoked Function Expression. Sigh :(
+var iife = function (endpoint) { return function(req, res) { handleHttpRequest(endpoint, req, res)}};
 for (var i = 0; i < endpoints.length; ++i)
 {
 	if (endpoints[i].method == "GET")
 	{
-		app.get(endpoints[i].url,function (j) { return function(req, res) { handleHttpRequest(endpoints[j], req, res)}}(i) );
+		app.get(endpoints[i].url, iife(endpoints[i]));
 	}
 	else if (endpoints[i].method == "POST")
 	{
-		app.post(endpoints[i].url, function (j) {return function(req, res) { handleHttpRequest(endpoints[j], req, res)}}(i) );
+		app.get(endpoints[i].url, iife(endpoints[i]));
 	}
 }
 
@@ -72,18 +68,37 @@ function handleHttpRequest(endpoint, req, res)
 			res.end(data);
 		});
 		
-		var cmd = endpoint.createCommand(req);
-		console.log("Writing command: " + cmd);
+		var cmd = endpoint.createCommand(req.params, req.body);
+		console.log("Executing command: " + cmd);
+		console.log("\nOn device with ID: " + deviceId)
 		deviceSocket.write(cmd);	
-		
 	}
 }
 
+app.use(express.static('public'));
 
+app.post('/:deviceId/name/:name', function(req, res) {
+	var deviceId = req.params.deviceId;
+	var name = req.params.deviceId;
+	
+	if (device_info[name] != undefined)
+	{
+		res.status(405).end();
+		return;
+	}
+	//When we assign a name to a device, add the same object under a different key
+	device_infos[deviceId].name = name;
+	device_infos[name] = device_infos[deviceId];
+	res.end();
+});
+app.get('/devices', function(req, res) {
+	res.end(JSON.stringify(device_infos));
+});
 
 var http_server = app.listen(8080, function(){
-    console.log("Started listening");			
+    console.log("Http Server Started");			
 });
+
 
 var device_infos = {};
 var device_sockets = {};
@@ -94,16 +109,18 @@ var socket_server = net.createServer(function (socket)
 		var device_info = JSON.parse(data);
 		//When the device first connects, it should send an object describing itself. name/type
 		socket.deviceId = device_info.deviceId;
-		device_infos[device_info.deviceId] = {type:device_info.type, deviceId:device_info.deviceId};
+		device_infos[device_info.deviceId] = {type:device_info.type, deviceId:device_info.deviceId, name:undefined};
 		device_sockets[device_info.deviceId] = socket;
 	});
 	
 	socket.on('close', function ()
 	{
+		var deviceInfo = device_infos[socket.deviceId];
 		device_infos[socket.deviceId] = undefined;
+		device_infos[deviceInfo.name] = undefined;
 	});
 });
 
-socket_server.listen(8081, function(){ console.log("Server Started")});
+socket_server.listen(8081, function(){ console.log("Socket Server Started")});
 
 
