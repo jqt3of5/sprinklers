d2f5b64612569481980ebd98940c11fa4fde9a18
jqt3of5 <jqt3of5@gmail.com>
2016-06-15 13:02:25 -0600

    Update IDevice.h

--- hal/photon/common/IDevice.h
+++ hal/photon/common/IDevice.h
@@ -11,4 +11,19 @@ public:
   {
     return spark_deviceID().c_str();
   }
+  
+  char * Serialize()
+  {
+    char * devTag = "deviceId";
+    char * deviceId = GetDeviceId();
+    char * typeTag = "type";
+    char * type = GetDeviceType();
+    int len = strlen(devTag) + strlen(deviceId) 
+            + strlen(typeTag) + strlen(type);
+    
+    char * result = new char[2*len];
+    sprintf(result, "{'%s':'%s', '%s':'%s'}", "type", type, "deviceId", deviceId);
+  
+    return result; 
+  }
 };
