11f5806415049504cec0290af3dad69e3e96e341
jqt3of5 <jqt3of5@gmail.com>
2016-06-15 12:53:05 -0600

    Update IDevice.h

--- hal/photon/common/IDevice.h
+++ hal/photon/common/IDevice.h
@@ -6,4 +6,9 @@ public:
   virtual char * ProcessData(char * data, int count) = 0;
 
   virtual char * GetDeviceType() = 0;
+  
+  char * GetDeviceId()
+  {
+    return spark_deviceID().c_str();
+  }
 };
